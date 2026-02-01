1b87d342ccf37ee9f6a12e61b4f46a27c035a334
Mateo Mansilla <109771313+MateoManDev@users.noreply.github.com>
2026-01-31 23:29:17 -0300

    git-pages6.0

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nombre-de-tu-repo/", // <--- ¡ESTO ES VITAL!
+  base: "/camiones/", // <--- ¡ESTO ES VITAL!
 });
